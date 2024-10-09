99787e79b732a3a43bcb10295f370e1c22ae6e6e
João Vitor <joao.vitor012020@gmail.com>
2024-10-09 01:48:00 -0300

    chore: config NextThemesProvider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,11 +2,14 @@
 
 import { NextUIProvider } from '@nextui-org/react';
 import { ReactNode } from 'react';
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <NextUIProvider>
-      {children}
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
     </NextUIProvider>
   )
 }
