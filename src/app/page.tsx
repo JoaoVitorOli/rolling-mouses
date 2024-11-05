f4393a1978472522e12980172ac680fb7fd746bf
João Vitor <joao.vitor012020@gmail.com>
2024-11-05 17:10:41 -0300

    wip: render CardHome and Divider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { CardHome } from "@/components/CardHome";
+import { Divider } from "@nextui-org/react";
+
 export default function Home() {
   return (
-    <div className="">
-
-    </div>
+    <section className="mt-20 pt-4">
+      <div className="px-4 container mx-auto flex flex-col gap-6">
+        <CardHome />
+        <Divider />
+        <CardHome />
+      </div>
+    </section>
   );
 }
